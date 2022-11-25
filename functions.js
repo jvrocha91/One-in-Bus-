f4e586eb85cdc5d80edced4a9751681507b039fd
Natcyka <119058648+Natcyka@users.noreply.github.com>
2022-11-25 17:46:26 -0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,8 +1,20 @@
-function generateRandomNumber(){
+﻿function generateRandomNumber(){
     const min=100000000;
     const max=999999999;
         var num=Math.floor(Math.random()*(max - min + 1) + min);
 return num;
+}
+
+function generateIdUso(){
+
+    const min=100;
+
+    const max=999;
+
+        var num=Math.floor(Math.random()*(max - min + 1) + min);
+
+return num;
+
 }
 
 function show_Id() {
@@ -12,7 +24,7 @@ function show_Id() {
     return numero;
     }
 
-    function cadastraBilhete() {
+  function cadastraBilhete() {
   
   let id = show_Id();
 
@@ -23,3 +35,110 @@ function show_Id() {
         id:id,           
     }));
 }
+
+function recarregabil() {
+    const codigoDatabase = document.getElementById('codigo_bilhete').value;
+    var menu = document.getElementById("Tipo");
+	var value = menu.value;
+	var tipo = menu.options[menu.selectedIndex].text;
+    console.log(codigoDatabase)
+    console.log(tipo)
+    let objRecarga = { cdb:codigoDatabase, tipo:tipo };
+    let url = 'http://localhost:4000/Recarga/'
+    let res = axios.post(url, objRecarga).then(response => {
+        if (response.data) {
+            showmessageSuccess();
+            const msg = new Comunicado(response.data.mensagem);
+            console.log(msg.get());
+        }
+    })
+        .catch(error => {
+            if (error.response) {
+                showmessageError();
+                const msg = new Comunicado
+                (error.reponse.data.mensagem);
+                console.log(msg.get());
+}
+        })
+}
+function cadastraUsos() {
+
+    const codigoDatabase = document.getElementById('codigo_bilhete1').value;
+    let idUso = generateIdUso;
+    console.log(codigoDatabase)
+
+    console.log(idUso)
+
+    let objUso = { cdb:codigoDatabase, idUso:idUso };
+
+    let url = 'http://localhost:4000/usoBilhete/'
+
+    let res = axios.post(url, objUso).then(response => {
+
+        if (response.data) {
+
+            showmessageSuccess();
+
+            const msg = new Comunicado(response.data.mensagem);
+
+            console.log(msg.get());
+
+        }
+
+    })
+
+        .catch(error => {
+
+            if (error.response) {
+
+                showmessageError();
+
+                const msg = new Comunicado
+
+                (error.reponse.data.mensagem);
+
+                console.log(msg.get());
+
+}
+
+        })
+
+}
+function pegaDataDePrimeiroUso()
+{
+    const num = document.getElementById('codigo_bilhete1').value;
+    let objUso = { cdb:num };
+
+    let url = 'http://localhost:4000/dataVencimento/'
+
+    let res = axios.post(url, objUso).then(response => {
+
+        if (response.data) {
+
+            console.log(reponse.data)
+            if (response.data.dataPrimeiroUso)
+
+        }
+
+    })
+
+        .catch(error => {
+
+            if (error.response) {
+
+                showmessageError();
+
+                const msg = new Comunicado
+
+                (error.reponse.data.mensagem);
+
+                console.log(msg.get());
+
+}
+
+        })
+
+
+}
+
+
--- index.js
+++ index.js
@@ -1,129 +1,292 @@
 const bodyParser = require('body-parser');
 const { Console } = require('console');
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const fs = require("fs");
+const { response } = require("express");
+const PORT = 4000;
 
-function banco_de_dados ()
-{
-	process.env.ORA_SDTZ = 'UTC-3'; 
-	
-	this.getConexao = async function ()
-	{
+
+function conectarNoBancoDeDados() {
+	process.env.ORA_SDTZ = 'UTC-3';
+
+	this.getConexao = async function () {
 		if (global.conexao)
 			return global.conexao;
 
-        const oracledb = require('oracledb');
-        const dbConfig = require('./oracleconfig.js');
-        
-        try
-        {
-		    global.conexao = await oracledb.getConnection(dbConfig);
+		const oracledb = require('oracledb');
+		const dbConfig = require('./oracleconfig.js');
+
+		try {
+			global.conexao = await oracledb.getConnection(dbConfig);
 		}
-		catch (erro)
-		{
-			console.log ('Cannot connect');
+		catch (erro) {
+			console.log('Cannot connect');
 			process.exit(1);
 		}
 
 		return global.conexao;
 	}
 
-	this.estrutureSe = async function ()
-	{
-		try
-		{
+	this.criadorDeTabelas = async function () {
+		try {
+			console.log("criando bilhete")
 			const conexao = await this.getConexao();
-			const sql     = 'CREATE TABLE BILHETE (ID_USUARIO_BILHETE NUMBER(9)CONSTRAINT PK_ID_USUARIO_BILHETE PRIMARY KEY,'+
-			'HORA_CRIACAO NVARCHAR2(8),'+
-			'DATA_CRIACAO DATE)';
+			const sql = 'CREATE TABLE BILHETE (ID_USUARIO_BILHETE NUMBER(9)CONSTRAINT PK_ID_USUARIO_BILHETE PRIMARY KEY,' +
+				'HORA_CRIACAO NVARCHAR2(8),' +
+				'DATA_CRIACAO DATE)';
 			await conexao.execute(sql);
-			console.log('Table created')
+			console.log('Table bilhete created')
 		}
-		catch (erro)
-		{} 
-	}
-}
+		catch (erro) {
+			console.log(erro)
+		}
+		try {
+			console.log("criando tipo_bilhete")
+			const conexao = await this.getConexao();
+			const sql = 'CREATE TABLE TIPO_BILHETE (TIPO_BILHETE NVARCHAR2(30)CONSTRAINT PK_TIPO_BILHETE PRIMARY KEY,' +
+				'HORA_RECARGA NVARCHAR2(5),' +
+				'DATA_RECARGA DATE,' +
+				'FK_ID_USUARIO_BILHETE NUMBER(9),' +
+				'FOREIGN KEY (FK_ID_USUARIO_BILHETE) references BILHETE(ID_USUARIO_BILHETE),' +
+				'UNIQUE (FK_ID_USUARIO_BILHETE))';
 
-function BILHETE (bd)
-{
-	this.bd = bd;
-	
-	this.inclua = async function (Bilhete)
-	{
+			await conexao.execute(sql);
+			console.log('Table Tipo bilhete created')
+		}
+		catch (erro) {
+			console.log(erro)
+		}
+		try {
+            console.log("criando uso do bilhete")
+            const conexao = await this.getConexao();
+            const sql = 'CREATE TABLE USO_DO_BILHETE (USO_DO_BILHETE NUMBER(3)CONSTRAINT PK_USO_DO_BILHETE PRIMARY KEY,' +
+                'HORA_DE_USO NVARCHAR2(5),' +
+                'DATA_DE_USO DATE,'+
+                'FK_ID_USUARIO_BILHETE NUMBER(9),' +
+                'FOREIGN KEY (FK_ID_USUARIO_BILHETE) references BILHETE(ID_USUARIO_BILHETE))';
 
-		const sql1 = "INSERT INTO BILHETE VALUES (:0,:1,:2)";
-		const dados = [( parseInt(Bilhete.cdb)),( Bilhete.horacri),( Bilhete.datacri)];
-	
-	
-		console.log(sql1, dados);
-		await conexao.execute(sql1, dados);//
-		
-		const sql2 = 'COMMIT';
-		await conexao.execute(sql2);	
-	}	
-}	
-
-
-function Bilhete (id,datac,horac)
-{
-	    this.cdb = id;
-	    this.datacri = new Date().toLocaleDateString();
-	    this.horacri = new Date().toLocaleTimeString();
-}
-
-function middleWareGlobal (req, res, next)
-{
-    console.time('Req'); 
-    console.log('method: '+req.method+'; URL: '+req.url); 
-
-    next(); 
-
-    console.log('END'); 
-
-    console.timeEnd('Req'); 
-}
-
-async function inclusao (req, res)
-{
-    const Bilhetecriado = new Bilhete (req.body.id,req.body.datac,req.body.horac);
-    try
-    {
-        await  global.Bilhetes.inclua(Bilhetecriado);
-		console.log('Insert suc');
-	}
-	catch (err)
-	{
-		console.log('Error in including');
-		console.log(err)
+            await conexao.execute(sql);
+            console.log('Table uso do bilhete created')
+        }
+        catch (erro) {
+            console.log(erro)
+        }
     }
 }
 
-async function server ()
+function inserirBilheteBanco(bd) {
+	this.bd = bd;
+
+	this.inclua = async function (Bilhete) {
+
+		const sql1 = "INSERT INTO BILHETE VALUES (:0,:1,:2)";
+		const dados = [(parseInt(Bilhete.cdb)), (Bilhete.horacri), (Bilhete.datacri)];
+
+
+		console.log(sql1, dados);
+		await conexao.execute(sql1, dados);
+
+		const sql2 = 'COMMIT';
+		await conexao.execute(sql2);
+	}
+}
+
+function inserirRecargaBilhete(bd) {
+	this.bd = bd;
+
+	this.insert_re = async function (get_selected_bil) {
+
+		const sql1 = "INSERT INTO TIPO_BILHETE VALUES (:0,:1,:2,:3)";
+		const dados = [(get_selected_bil.tipo), (get_selected_bil.horacri), (get_selected_bil.datacri), (parseInt(get_selected_bil.cdb))];
+
+		console.log(sql1, dados);
+		await conexao.execute(sql1, dados);
+
+		const sql2 = 'COMMIT';
+		await conexao.execute(sql2);
+	}
+}
+
+function inserirUsosDoBilhete(bd) {
+    this.bd = bd;
+
+    this.insert_use = async function (getUsoBilhete) {
+
+        const sql1 = "INSERT INTO USO_DO_BILHETE VALUES (:0,:1,:2,:3)";
+        const dados = [(getUsoBilhete.idUso), (getUsoBilhete.horacri), (getUsoBilhete.datacri), (parseInt(getUsoBilhete.cdb))];
+
+        console.log(sql1, dados);
+        await conexao.execute(sql1, dados);
+
+        const sql2 = 'COMMIT';
+        await conexao.execute(sql2);
+    }
+}
+function getDataPrimeiroUso(bd) {
+    this.bd = bd;
+
+    this.Find_Fist_Date = async function (codigo) {
+
+        const dataPrimeiroUso = 'select Min(DATA_DE_USO) from USO_DO_BILHETE where USO_DO_BILHETE.FK_ID_USUARIO_BILHETE =:0 ';
+
+        console.log(dataPrimeiroUso);
+        return await conexao.execute (dataPrimeiroUso, [(codigo)])
+
+    }
+}
+function encontrarTipoDoBIlhete(bd)
 {
-    const bd = new banco_de_dados ();
-	await bd.estrutureSe();
-    global.Bilhetes = new BILHETE (bd);
+	this.bd=bd;
 
-    const express = require('express');
-    const app     = express();
-	const cors    = require('cors')
-    const bodyParser = require("body-parser");
-	const fs = require("fs");
-	const { response } = require("express");
-	const PORT = 4000;
+	this.findBilheteType = async function(codigo){
 
-    app.use(express.json());   
-	app.use(cors()); 
-    app.use(middleWareGlobal); 
+		const bilheteType = 'select tipo_bilhete.TIPO_BILHETE FROM BILHETE JOIN TIPO_BILHETE on BILHETE.ID_USUARIO_BILHETE = tipo_bilhete.FK_ID_USUARIO_BILHETE where bilhete.FK_ID_USUARIO_BILHETE=:0';
+		switch (bilheteType)
+		{
+			case 'Bilhete Único':
+				bilheteType = 40
+				break;
+			case 'Bilhete duplo':
+				bilheteType = 80
+				break;
+			case '7 dias':
+				bilheteType = 7
+				break;
+			case '30 dias':
+				bilheteType = 30
+				break;
+				default:
+
+		}
+		console.log(bilheteType);
+		return await conexao.execute(bilheteType, [(codigo)])
+	}
+	
+}
+
+async function inclusaoRecarga(req, res) {
+	const Recarga = new getSelectBil(req.body.tipo, req.body.cdb)
+	console.log(Recarga)
+	try {
+		await global.recargas.insert_re(Recarga);
+		console.log('Insert working');
+	}
+	catch (err) {
+		console.log('Error in inclusaoRecarga');
+		console.log(err)
+	}
+}
+async function pegarDataVencimento(req, res) {
+	const codigo = req.body.cdb
+	console.log(codigo)
+	try {
+		await global.Data.Find_Fist_Date(codigo);
+		console.log('Insert working');
+	}
+	catch (err) {
+		console.log('Error in dataVencimento');
+		console.log(err)
+	}
+}
+async function pegarTipoBilhete(req, res) {
+	const codigo = req.body.cdb
+	console.log(codigo)
+	try {
+		await global.tipo.findBilheteType(codigo);
+		console.log('Insert working');
+	}
+	catch (err) {
+		console.log('Error in pegarTipoBilhete');
+		console.log(err)
+	}
+}
+
+function getSelectBil(tipo, cdb) {
+	this.tipo = tipo;
+	this.cdb = cdb;
+	this.datacri = new Date().toLocaleDateString();
+	this.horacri = new Date().toLocaleTimeString();
+	console.log(tipo)
+}
+
+function Bilhete(id, datac, horac) {
+	this.cdb = id;
+	this.datacri = new Date().toLocaleDateString();
+	this.horacri = new Date().toLocaleTimeString();
+}
+
+function getUsoBilhete(idUso, cdb) {
+    this.idUso
+    this.cdb = cdb;
+    this.datacri = new Date().toLocaleDateString();
+    this.horacri = new Date().toLocaleTimeString();
+    console.log(idUso)
+}
+
+function middleWareGlobal(req, res, next) {
+	console.time('Req');
+	console.log('method: ' + req.method + '; URL: ' + req.url);
+
+	next();
+
+	console.log('END');
+
+	console.timeEnd('Req');
+}
+
+async function inclusaoBilhete(req, res) {
+	const Bilhetecriado = new Bilhete(req.body.id, req.body.datac, req.body.horac);
+	try {
+		await global.Bilhetes.inclua(Bilhetecriado);
+		console.log('Insert suc');
+	}
+	catch (err) {
+		console.log('Error in including');
+		console.log(err)
+	}
+}
+async function inclusaoUso(req, res) {
+    const usoBilhete = new getUsoBilhete(req.body.idUso, req.body.cdb)
+    console.log(usoBilhete)
+    try {
+        await global.usos.insert_use(usoBilhete);
+        console.log('Insert working');
+    }
+    catch (err) {
+        console.log('Error in inclusaoUso');
+        console.log(err)
+    }
+}
+
+async function abrirServer() {
+	const bd = new conectarNoBancoDeDados();
+	await bd.criadorDeTabelas();
+	global.Bilhetes = new inserirBilheteBanco(bd);
+	global.recargas = new inserirRecargaBilhete(bd);
+	global.usos = new inserirUsosDoBilhete(bd);
+	global.data = new getDataPrimeiroUso(bd);
+	global.tipo = new encontrarTipoDoBIlhete(bd);
+
+	app.use(express.json());
+	app.use(cors());
+	app.use(middleWareGlobal);
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use((req, res, next) => {
 		res.header("Access-Control-Allow-Origin", "*");
 		app.use(cors());
 		next();
-	  });
+	});
 
-    app.post  ('/Bilhete'        , inclusao); 
-
-    console.log ('Server port 4000')
-    app.listen(4000);
+	// cria o endpoint POST /Bilhete e /Recarga
+	app.post('/Bilhete', inclusaoBilhete);
+	app.post('/Recarga', inclusaoRecarga);
+	app.post('/usoBilhete', inclusaoUso);
+	app.post('/dataVencimento',pegarDatavencimento);
+	app.post('/tipoBilhete', findBilheteType)
+	console.log('Server port ' + PORT);
+	app.listen(PORT);
 }
 
-server();
+abrirServer();
